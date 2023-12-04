05f1531fb8f5890f99bbfcaedbb57bfd5c2edf15
ShadowRCX <TheShadowCorporation@proton.me>
2023-12-03 19:06:45 -0500

    Initial Commit

--- src/main/java/net/shadowrcx/alltheadditions/AllTheAdditions.java
+++ src/main/java/net/shadowrcx/alltheadditions/AllTheAdditions.java
@@ -31,7 +31,6 @@ public class AllTheAdditions {
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
-        // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
     }
